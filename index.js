80fd7bec1d0f767ce7781985feb74967daf5488b
Jonathan Hansen <jonathanhansen808@gmail.com>
2022-04-07 01:33:30 -0500

    Made left sidebar Bulma/resized search

--- index.js
+++ index.js
@@ -102,7 +102,32 @@ admin_signinModalBg.addEventListener('click', () => {
     admin_signinModal.classList.remove('is-active');
 });
 
+// Left side buttons 
+const dropdowns = document.querySelectorAll('.dropdown:not(.is-hoverable)');
 
+if (dropdowns.length > 0) {
+    // For each dropdown, add event handler to open on click.
+    dropdowns.forEach(function (el) {
+        el.addEventListener('click', function (e) {
+            e.stopPropagation();
+            el.classList.toggle('is-active');
+        });
+    });
+
+    // If user clicks outside dropdown, close it.
+    document.addEventListener('click', function (e) {
+        closeDropdowns();
+    });
+}
+
+/*
+ * Close dropdowns by removing `is-active` class.
+ */
+function closeDropdowns() {
+    dropdowns.forEach(function (el) {
+        el.classList.remove('is-active');
+    });
+}
 
 let about = document.querySelector("#about");
 about.addEventListener("click", (e) => {
